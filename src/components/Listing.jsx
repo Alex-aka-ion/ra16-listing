56558adef148311483b3c6e5cc87800566168793
Alexey Yakushin <alex.aka.ion@gmail.com>
2020-11-29 21:31:34 +0300

    cs

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -4,7 +4,7 @@ import Item from "./Item";
 export default function Listing({items = []}) {
     return (
         <div className="item-list">
-            {items.map((e) => <Item item={e} key={e.listing_id} /> )}
+            {items.map((e) => <Item item={e} key={e.listing_id}/>)}
         </div>
     );
 }
